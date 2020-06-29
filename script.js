75851ec3675aac5991336b533ca22558dcee33f9
epedalino <epedalino@gmail.com>
2020-06-29 18:42:02 -0400

    Ability to select and play an audio file from computer POC

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+/**
+ * Represents a book.
+ * @param {string} title - The title of the book.
+ * @param {string} author - The author of the book.
+ * @returns {object} The object that represents the book.
+ */
+function createNewBook(title, author) {
+}
+
 const songs = [
   "songs/bensound-creativeminds.mp3",
   "songs/bensound-cute.mp3",
@@ -5,50 +14,28 @@ const songs = [
   "songs/bensound-happyrock.mp3",
   "songs/bensound-inspire.mp3"
 ];
+/**
+ *
+ * @param {HTMLElement} fileInput - File Input for audio files
+ * @returns {string} location of the file in the input
+ */
+function getLocationFromFileInput(fileInput){
+  if (!fileInput.files.length) return;
 
-function changeHandler({
-  target
-}) {
-  // Make sure we have files to use
-  if (!target.files.length) return;
-
-  // Create a blob that we can use as an src for our audio element
-  const urlObj = URL.createObjectURL(target.files[0]);
-
-  // Create an audio element
-  const audio = document.createElement("audio");
-
-  // Clean up the URL Object after we are done with it
-  audio.addEventListener("load", () => {
-    URL.revokeObjectURL(urlObj);
-  });
-
-  // Append the audio element
-  document.body.appendChild(audio);
-
-  // Set the src and start loading the audio from the file
-  audio.src = urlObj;
+  const urlObj = URL.createObjectURL(fileInput.files[0]);
+  console.log (urlObj)
+  return urlObj
 }
-
-document
-  .getElementById("audio-upload")
-  .addEventListener("change", changeHandler);
-
 const list = document.getElementById('songList');
-
-function addSongToList(listItem) {
-  list.appendChild(listItem);
-}
-
 function createSongListItem(title, location) {
-  const item = document.createElement('li');
-  const titleElement = document.createTextNode(title);
-  item.appendChild(titleElement);
-  item.dataset.location = location;
-  return item;
-}
+    const item = document.createElement('li');
+    const titleElement = document.createTextNode(title);
+    item.appendChild(titleElement);
+    item.dataset.location = location;
+    return item;
+  }
 
-const createSongList = () => {
+function createSongList() {
   for(let i = 0; i<songs.length; i++) {
     const title = songs[i];
     const location = songs[i];
@@ -58,38 +45,60 @@ const createSongList = () => {
 }
 createSongList();
 
-songList.onclick = (e) => {
-  const clickedItem = e.target
+function addSongToList(listItem) {
+  //createSongListItem(newInput[0].name)
+  list.appendChild(listItem);
+}
+
+let input = document.getElementById("fileUpload")
+input.addEventListener("change", function(e) {
+  if (e.target.files.length > 0) {
+    Array.from(e.target.files).forEach((file) => {
+      fileUpload.innerHTML += '<br>' + file.name;
+      let newInputFiles = e.target.files
+      console.log(newInputFiles)
+      const title = newInputFiles[0].name;
+      console.log(e.target);
+      const location = getLocationFromFileInput(e.target)
+      const item = createSongListItem(title, location);
+      addSongToList(item);
+    })
+  }
+})
+
+const displayCurrentPlayingTrack = (e) => {
+  const clickedItem = e.target;
   const location = clickedItem.dataset.location;
-  const source = document.getElementById('source')
-  source.src = location
-  // 'songs/' + clickedItem.innerText
+  console.log("location", location);
+  const source = document.getElementById('source');
+  source.src = location;
 
   document.getElementById('currentlyPlayingSong').innerText= "Currently Playing: "
   document.getElementById('currentSong').innerText=clickedItem.innerText
   player.load()
   player.play()
 }
+songList.onclick = displayCurrentPlayingTrack;
 
-const playAudio = () => {
-  if(player.readyState) {
-    player.play()
+  const playAudio = () => {
+    if(player.readyState) {
+      player.play()
+    }
   }
-}
 
-const pauseAudio = () => {
-  player.pause()
-}
-
-const slider = document.getElementById('volumeSlider')
-slider.oninput = (e) => {
-  const volume = e.target.value
-  player.volume = volume
-}
-
-const updateProgress = () => {
-  if(player.currentTime > 0) {
-    const progressBar = document.getElementById('progress')
-    progressBar.value = (player.currentTime / player.duration) * 100
+  const pauseAudio = () => {
+    player.pause()
   }
-}
+
+  const slider = document.getElementById('volumeSlider')
+  slider.oninput = (e) => {
+    const volume = e.target.value
+    player.volume = volume
+  }
+
+  const updateProgress = () => {
+    if(player.currentTime > 0) {
+      const progressBar = document.getElementById('progress')
+      progressBar.value = (player.currentTime / player.duration) * 100
+    }
+  }
