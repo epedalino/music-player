a9d4c3df5419f6a8826609ac085ba5fb4b828137
epedalino <epedalino@gmail.com>
2020-07-04 18:10:54 -0400

    Reorganize code - Add jsdoc comments

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-/**
- * Represents a book.
- * @param {string} title - The title of the book.
- * @param {string} author - The author of the book.
- * @returns {object} The object that represents the book.
- */
-function createNewBook(title, author) {
-}
-
 const songs = [
   "songs/bensound-creativeminds.mp3",
   "songs/bensound-cute.mp3",
@@ -14,19 +5,14 @@ const songs = [
   "songs/bensound-happyrock.mp3",
   "songs/bensound-inspire.mp3"
 ];
-/**
- *
- * @param {HTMLElement} fileInput - File Input for audio files
- * @returns {string} location of the file in the input
- */
-function getLocationFromFileInput(fileInput){
-  if (!fileInput.files.length) return;
 
-  const urlObj = URL.createObjectURL(fileInput.files[0]);
-  console.log (urlObj)
-  return urlObj
-}
 const list = document.getElementById('songList');
+
+/**createSongLingItem function creates song list items
+ * @param {string} title - The title of the song.
+ * @param {string} location - The song directory location.
+ * @returns {HTMLElement} item - Title and location of the song
+ */
 function createSongListItem(title, location) {
     const item = document.createElement('li');
     const titleElement = document.createTextNode(title);
@@ -35,30 +21,41 @@ function createSongListItem(title, location) {
     return item;
   }
 
-function createSongList() {
-  for(let i = 0; i<songs.length; i++) {
-    const title = songs[i];
-    const location = songs[i];
-    const item = createSongListItem(title, location);
-    addSongToList(item);
+/**
+ * createSongList function creates the song list based on static array of songs
+ * @param {string} title - The title of the song.
+ * @param {string} location - The song directory location.
+ * @param {HTMLElement} item - Title and location of a song.
+ */
+  function createSongList() {
+    for(let i = 0; i<songs.length; i++) {
+      const title = songs[i];
+      const location = songs[i];
+      const item = createSongListItem(title, location);
+      addSongToList(item);
+    }
   }
-}
-createSongList();
+  createSongList();
 
-function addSongToList(listItem) {
-  //createSongListItem(newInput[0].name)
-  list.appendChild(listItem);
-}
+/**
+ * The addSongToList function appends a new listItem to the existing song list
+ */
+  function addSongToList(listItem) {
+    list.appendChild(listItem);
+  }
 
+/**
+ * Event listener that identifies a change or new song fileUpload.
+ * Extracts title and location of the new item and displays the info.
+ * @param {HTMLElement} input - new user selected file audio input.
+ */
 let input = document.getElementById("fileUpload")
 input.addEventListener("change", function(e) {
   if (e.target.files.length > 0) {
     Array.from(e.target.files).forEach((file) => {
       fileUpload.innerHTML += '<br>' + file.name;
       let newInputFiles = e.target.files
-      console.log(newInputFiles)
       const title = newInputFiles[0].name;
-      console.log(e.target);
       const location = getLocationFromFileInput(e.target)
       const item = createSongListItem(title, location);
       addSongToList(item);
@@ -66,39 +63,68 @@ input.addEventListener("change", function(e) {
   }
 })
 
+/**
+ * getLocationFromFileInput identifies the location URL of the song file input
+ * @param {string} fileInput - File Input for audio files.
+ * @returns {urlObj} - Blob oject representing URL location.
+ */
+function getLocationFromFileInput(fileInput){
+  if (!fileInput.files.length) return;
+
+  const urlObj = URL.createObjectURL(fileInput.files[0]);
+  return urlObj;
+}
+
+/**
+ * displayCurrentPlayingTrack function provides a visual display of the
+ * selected song and its location.
+ * @param {HTMLElement} location - name and location of the clicked file.
+ */
 const displayCurrentPlayingTrack = (e) => {
   const clickedItem = e.target;
   const location = clickedItem.dataset.location;
-  console.log("location", location);
   const source = document.getElementById('source');
   source.src = location;
 
-  document.getElementById('currentlyPlayingSong').innerText= "Currently Playing: "
-  document.getElementById('currentSong').innerText=clickedItem.innerText
-  player.load()
-  player.play()
+  document.getElementById('currentlyPlayingSong').innerText= "Currently Playing: ";
+  document.getElementById('currentSong').innerText=clickedItem.innerText;
+  player.load();
+  player.play();
 }
 songList.onclick = displayCurrentPlayingTrack;
 
+/**
+ * Custom control to play the selected song
+ */
+
   const playAudio = () => {
     if(player.readyState) {
-      player.play()
+      player.play();
     }
   }
 
+/**
+ * Custom control to pause the selected song.
+ */
   const pauseAudio = () => {
-    player.pause()
+    player.pause();
   }
 
-  const slider = document.getElementById('volumeSlider')
+/**
+ * Volume slider to aid user in increasing or decreasing volume.
+ */
+  const slider = document.getElementById('volumeSlider');
   slider.oninput = (e) => {
-    const volume = e.target.value
-    player.volume = volume
+    const volume = e.target.value;
+    player.volume = volume;
   }
 
+/**
+ * Custom progress bar to indicate how much of the song the users has listened to.
+ */
   const updateProgress = () => {
     if(player.currentTime > 0) {
-      const progressBar = document.getElementById('progress')
-      progressBar.value = (player.currentTime / player.duration) * 100
+      const progressBar = document.getElementById('progress');
+      progressBar.value = (player.currentTime / player.duration) * 100;
     }
   }
